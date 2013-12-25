9cc8e057a6f9fef6bd586b91e4eeb86291114bc5
Jesse Collis <jesse@jcmultimedia.com.au>
2013-12-26 09:46:41 +1100

    Added the Service/characteristics

--- HeartRateMonitor/HeartRateMonitor/JCViewController.m
+++ HeartRateMonitor/HeartRateMonitor/JCViewController.m
@@ -8,6 +8,18 @@
 
 #import "JCViewController.h"
 
+// https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx
+
+#define HRM_DEVICE_INFO_SERVICE @"180A" //org.bluetooth.service.device_information
+#define HRM_HEAT_RATE_SERVICE @"180D" //org.bluetooth.service.heart_rate
+
+// https://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicsHome.aspx
+
+#define HRM_MEASUREMENT_CHARACTERISTIC @"2A37" //org.bluetooth.characteristic.heart_rate_measurement
+#define HRM_BODY_LOCATION_CHARACTERISTIC @"2A38" //org.bluetooth.characteristic.blood_pressure_measurement
+#define HRM_MANUFACTURER_NAME_CHARACTERISTIC @"2A29" //org.bluetooth.characteristic.manufacturer_name_string
+
+
 @interface JCViewController ()
 
 @end
